8b4caab0010bb887c380228dba87a868ddfc00df
JAMINIZ <pjw4509@naver.com>
2024-05-02 17:32:23 +0900

    react 1

---
+++ src/01/Hello.js
@@ -0,0 +1,9 @@
+function Hello() {
+    return (
+        <p>
+        K-digital 7기 박정우
+      </p>
+    );
+}
+
+export default Hello;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './01/Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello />
       </header>
     </div>
   );
 }
 
 export default App;
+// react는 자바스크립트를 짤 때 반드시 return 을 짠다 
+//＃ .js 파일에는 function 하나만 들어가게  
+//＃ function 이름은 반드시 대문자로 시작
+// return 에는 우리가 아는 태그를 쓴다.
+// ＃ 부모노드 (태그 하나만) return 한다  
+// 자식노드에는 태그 여러개 해도 상관 없다.
+// import를 했으면 export deafault 를 해줘야 함 index에서 받아온 App 를 export!
+// class 가 아닌 className 으로 작성 카멜 표기법 사용
